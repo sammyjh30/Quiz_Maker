cf2ae01b587f4b3d6340622dcd1c9d3d9931f033
Kevin Julius <kevinjulius97@gmail.com>
2020-07-01 02:10:35 +0200

    added raw auth components to model

---
+++ QuizMakerAngular/src/app/components/log-in/log-in.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+
+export class LogInComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit() { }
+
+}
