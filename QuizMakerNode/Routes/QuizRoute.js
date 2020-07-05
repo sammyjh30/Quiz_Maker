4b7bd776473d65b1bf5f63d19b542d3aafaa0c50
Callum <callum.m.dunne@gmail.com>
2020-07-05 15:54:48 +0200

    Added in the get function and added in stuff for angular to use it

--- QuizMakerNode/Routes/QuizRoute.js
+++ QuizMakerNode/Routes/QuizRoute.js
@@ -14,20 +14,20 @@ router.post('/addQuestion',function(req,res){
         request.input('questionType', conn.sql.Int , req.body.questionType)
         request.input('questionNumber', conn.sql.Int , req.body.questionNumber)
         request.input('text', conn.sql.VarChar , req.body.text)
-        request.input('correctAnswer', conn.sql.Int , req.body.correctAnswer)
+        request.input('correctAnswer', conn.sql.Bit , req.body.correctAnswer)
         request.input('rightAnswer', conn.sql.VarChar , req.body.rightAnswer)
         request.input('wrongAnswer1', conn.sql.VarChar , req.body.wrongAnswer1)
         request.input('wrongAnswer2', conn.sql.VarChar , req.body.wrongAnswer2)
         request.input('wrongAnswer3', conn.sql.VarChar , req.body.wrongAnswer3)
         request.execute(`InsertQuestion`).then(() => {
-            res.status(201).send(messages[201])
+            res.status(201).send({'message' : messages[201]})
         }).catch((err) => {
             console.log(err)
-            res.status(500).send(messages[500])
+            res.status(500).send({'error':messages[500]})
         })
     }).catch((err) => {
         console.log(err)
-        res.status(500).send(messages[500])
+        res.status(500).send({'error':messages[500]})
     })
 })
 
@@ -37,15 +37,55 @@ router.delete('/deleteQuestion/:questionID',function(req,res){
         request.input('questionID', req.params.questionID)
         request.query(`DELETE Questions
         WHERE QuestionID =@questionID`).then(() => {
-            res.status(200).send(messages[200])
+            res.status(200).send({'message' : messages[200]})
         }).catch((err) => {
             console.log(err)
-            res.status(500).send(messages[500])
+            res.status(500).send({'error':messages[500]})
         })
     }).catch((err) => {
         console.log(err)
-        res.status(500).send(messages[500])
+        res.status(500).send({'error':messages[500]})
     })
+})
+
+router.get('/getQuestions/:quizId', function(req,res){
+    conn.poolPromise.then((pool)=>{
+        const request = pool.request();
+        request.input('quizId',req.params.quizId)
+        request.query(`SELECT Questions.questionId,roundNumber,questionType,questionNumber,correctAnswer,TFQuestions.[text] as TFText,MultipleChoiceQuestions.[text] as multiText, rightAnswer,wrongAnswer1,wrongAnswer2,wrongAnswer3  FROM Questions 
+        LEFT JOIN TFQuestions ON Questions.questionId = TFQuestions.questionId
+        LEFT JOIN MultipleChoiceQuestions ON Questions.questionId = MultipleChoiceQuestions.questionId
+        WHERE quizId = @quizId
+        ORDER BY roundNumber, questionNumber`).then((data) => {
+            res.status(200).send(data.recordset)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({'error':messages[500]})
+        })
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send({'error':messages[500]})
+    })
+})
+
+router.get('/getQuestions/:questionId',function(req,res){
+    conn.poolPromise.then((pool)=>{
+        const request = pool.request();
+        request.input('questionId',req.params.questionId)
+        request.query(`SELECT Questions.questionId,roundNumber,questionType,questionNumber,correctAnswer,TFQuestions.[text] as TFText,MultipleChoiceQuestions.[text] as multiText, rightAnswer,wrongAnswer1,wrongAnswer2,wrongAnswer3  FROM Questions 
+        LEFT JOIN TFQuestions ON Questions.questionId = TFQuestions.questionId
+        LEFT JOIN MultipleChoiceQuestions ON Questions.questionId = MultipleChoiceQuestions.questionId
+        WHERE Questions.questionId = @questionId
+        ORDER BY roundNumber, questionNumber`).then((data) => {
+            res.status(200).send(data.recordset)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({'error':messages[500]})
+        })
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send({'error':messages[500]})
+    })    
 })
 //#endregion
 
@@ -59,14 +99,14 @@ router.post('/addQuiz',function(req,res){
         request.input('startDateTime',conn.sql.DateTime,req.body.startDateTime)
         request.query(`INSERT INTO Quiz(quizName,HostId,startDateTime) 
         VALUES (@quizName,@hostId,@startDateTime)`).then(()=> {
-            res.status(201).send(messages[201])
+            res.status(201).send({'message' : messages[201]})
         }).catch((err) => {
             console.log(err)
-            res.status(500).send(messages[500])
+            res.status(500).send({'error':messages[500]})
         })
     }).catch((err) => {
         console.log(err)
-        res.status(500).send(messages[500])
+        res.status(500).send({'error':messages[500]})
     })
 })
 
@@ -76,15 +116,47 @@ router.delete('/deleteQuiz/:quizId',function(req,res){
         request.input('quizId', req.params.quizId)
         request.query(`DELETE Quiz
         WHERE quizId =@quizId`).then(() => {
-            res.status(200).send(messages[200])
+            res.status(200).send({'message' : messages[200]})
         }).catch((err) => {
             console.log(err)
-            res.status(500).send(messages[500])
+            res.status(500).send({'error':messages[500]})
         })
     }).catch((err) => {
         console.log(err)
-        res.status(500).send(messages[500])
+        res.status(500).send({'error':messages[500]})
     })
+})
+
+router.get('/getQuiz/:quizId',function(req,res){
+    conn.poolPromise.then((pool)=>{
+        const request = pool.request();
+        request.input('quizId',req.params.quizId)
+        request.query(`SELECT * FROM quiz WHERE quizId = @quizId`).then((data) => {
+            res.status(200).send(data.recordset)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({'error':messages[500]})
+        })
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send({'error':messages[500]})
+    })    
+})
+
+router.get('/getQuizByHostId/:hostId',function(req,res){ //can be change to be like host ID to make life easier if wanted 
+    conn.poolPromise.then((pool)=>{
+        const request = pool.request();
+        request.input('hostId',req.params.hostId)
+        request.query(`SELECT * FROM quiz WHERE hostId =  @hostId`).then((data) => {
+            res.status(200).send(data.recordset)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({'error':messages[500]})
+        })
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send({'error':messages[500]})
+    })    
 })
 //#endregion
 
