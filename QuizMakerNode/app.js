3a02fe3a85366148349e299e64b128e7d38610c5
Kevin Julius <kevinjulius97@gmail.com>
2020-07-08 23:20:17 +0200

    converted mailer to router

--- QuizMakerNode/app.js
+++ QuizMakerNode/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-var cors = require('cors');
-
+const cors = require('cors');
 const app = express();
+const config = require('./config');
+const auth = require('./middleware/auth');
+const conn = require('./services/dbconnection.js');
 const quizRouter = require('./routes/QuizRoute')
 const userRouter = require('./routes/UserRoute')
-
-const config = require('./config');
-
-const conn = require('./services/dbconnection.js');
-const auth = require('./middleware/auth');
-
-const mailer = require('./services/mailer');
+const mailer = require('./routes/MailerRoute');
 
 app.use(cors());
 
+//app.use(auth);  // enable request authentication
 app.use(express.json());
-app.use('/quiz',quizRouter)//how to add a route to the system
-app.use('/user',userRouter)
-app.get('/', function (req, res) {
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Quiz Maker Backend</h1>")
+});
+
+app.use('/quiz', quizRouter);  //how to add a route to the system
+app.use('/user', userRouter);
+app.use('/mailer', mailer);
+
+app.get('/testDB', function (req, res) {
   conn.poolPromise.then((pool) => { //make sure connection is made 
     pool.request().query('SELECT 1') //use connection to make request 
       .then(result => {
@@ -28,26 +33,6 @@ app.get('/', function (req, res) {
         throw err
       });
   }).catch(err => res.status(500).send(err)) //do shit with error
-});
-
-app.use("/hackerman", auth);
-
-app.get('/hackerman', function (req, res) {
-  res.status(200).send('hackerman you got in!')
-});
-
-app.use("/sendemail", auth);
-app.use("/sendemail", bodyParser.json());
-app.use("/sendemail", express.json());
-app.post('/sendemail', function (req, res) {
-  var mailOptions = req.body;
-  mailer.transport.sendMail(mailOptions, function (err, info) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(info);
-    }
-  });
 });
 
 app.listen(config.web.port, function () {
