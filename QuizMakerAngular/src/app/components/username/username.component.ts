8933462211503a4df2dbaeca8e7ad14d8cb1bd12
RIBAJACOB <RIBAJACOB@users.noreply.github.com>
2020-07-09 23:38:57 +0200

    updated functionality for editing user details

--- QuizMakerAngular/src/app/components/username/username.component.ts
+++ QuizMakerAngular/src/app/components/username/username.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { UserService } from "../../services/user.service";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-username',
@@ -12,7 +13,9 @@ export class UsernameComponent implements OnInit {
   detailsForm: FormGroup;
   isSubmitted  =  false;
   get formControls() { return this.detailsForm.controls; }
-  constructor(private formBuilder: FormBuilder, public userService:UserService) { }
+  constructor(private formBuilder: FormBuilder, 
+              public userService:UserService,
+              public authService: AuthService) { }
 
   ngOnInit(): void {
     this.detailsForm  =  this.formBuilder.group({
@@ -23,12 +26,12 @@ export class UsernameComponent implements OnInit {
   }
 
   updateUserProfile(data){
-
+    console.log(this.authService.userData.uid );
     console.log(data.firstname);
     console.log(data.lastname);
     console.log(data.email);
 
-    this.userService.updateUser('321',data.firstname,data.lastname,data.email);
+    this.userService.updateUser(this.authService.userData.uid ,data.firstname,data.lastname,data.email);
 
   }
 
