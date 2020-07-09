d2e71def19497348f7780aaecf4f888b47ab975c
Kevin Julius <kevinjulius97@gmail.com>
2020-07-09 15:37:28 +0200

    added files again

---
+++ QuizMakerAngular/src/app/models/teamUser.ts
@@ -0,0 +1,12 @@
+import { User } from './user';
+import { TeamMember } from './teammember';
+
+export class TeamUser implements User, TeamMember  {
+  teamId?: number;
+  userId?: string;
+  captain?: boolean;
+  email?: string;
+  name?: string;
+  surname?: string;
+
+}
