249cd72c87bed97a19f0a9d55ad4ecfdf510fdf0
Harvard007 <dircoliebenberg@gmail.com>
2020-07-10 00:21:20 +0200

    Added styles for Login, sign-up, forgot password and verify password

--- QuizMakerAngular/src/app/components/landing-page/landing-page.component.ts
+++ QuizMakerAngular/src/app/components/landing-page/landing-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
+
 })
+
+
+
+
 export class LandingPageComponent implements OnInit {
+  public slides = [
+    { src: '/assets/img/one.jpg' },
+    { src: '/assets/img/two.jpg' },
+    { src: '/assets/img/three.jpg' },
+    { src: '/assets/img/four.jpg' }
+  ];
+
+  currentSlide = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
